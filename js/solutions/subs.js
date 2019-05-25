5a843f0af505a2e92e947011174d2433e1f68626
Cameron Earl <developer.cameron@gmail.com>
2019-05-24 19:24:27 -0700

    Remove compiled js from repository

--- js/solutions/subs.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var helpers_1 = require("../helpers");
-exports.default = (function (data) {
-    var _a = data.split(/\s/g), na = _a[0], motif = _a[1];
-    return helpers_1.motifLocations(na, motif).join(' ');
-});
